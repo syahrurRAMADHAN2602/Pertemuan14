5b87ac7883ba7747649da4b328fd6ebf998935a5
PemudaTampan555 <flash24576@gmail.com>
2025-11-27 08:52:18 +0700

    Percobaan 2.2 (Perbaikan)

--- Kafe23.java
+++ Kafe23.java
@@ -3,6 +3,13 @@ package Pertemuan14;
 public class Kafe23 {
 
     public static void Menu(String namaPelanggan, boolean isMember) {
+        System.out.println("Selamat datang, " + namaPelanggan + "!");
+
+        if (isMember) {
+            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        }
+
+
         System.out.println("==== MENU RESTO KAFE ====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Cappuccino - Rp 20,000");
